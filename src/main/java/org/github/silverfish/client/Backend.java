13b4dd9dbfc31fb76a3de8132e90571e0fa7d2e6
Iftekharul Haque <iftekharul.haque@gmail.com>
2016-02-25 17:02:26 +0100

    Backend Interface: Some additional documentation

--- src/main/java/org/github/silverfish/client/Backend.java
+++ src/main/java/org/github/silverfish/client/Backend.java
@@ -6,9 +6,9 @@ import java.util.Map;
 public interface Backend<E> {
 
     /**
-     * Put <code>items</code> to the queue.
+     * Send <code>items</code> to the queue, to be enqueued.
      *
-     * @param items items to put
+     * @param items list of items to send, just data
      * @return number of added items
      */
     int enqueue(List<E> items);
@@ -16,41 +16,41 @@ public interface Backend<E> {
     /**
      * Takes <code>count</code> elements from the queue.
      *
-     * @param count how many elements to take
-     * @param blocking wait until some data
-     * @return map from identifier of payload item to the payload item
+     * @param count how many elements to take from the queue
+     * @param blocking blocking behaviour; if true, it will wait for data to appear in the queue, if false, it will immediately return on an empty queue
+     * @return the data you asked for; keyed on payload UUID, with the payload in the value
      */
     Map<String, E> dequeue(int count, boolean blocking);
 
     /**
      * Mark elements as processed.
      *
-     * @param ids which elements to mark
+     * @param ids list of UUIDs to mark as processed
      */
     void markProcessed(List<String> ids);
 
     /**
      * Mark elements as failed.
      *
-     * @param ids which elements to mark
+     * @param ids list of UUIDs to mark as failed
      */
     void markFailed(List<String> ids);
 
     /**
-     * Get last <code>limit</code> elements.
+     * Get the last <code>limit</code> elements from the queue. It is just a way of sampling the data.
      *
-     * @param limit elements count to return
-     * @return last elements.
+     * @param limit element count to return
+     * @return last <code>limit</code> elements
      */
     List<E> peek(int limit);
 
     /**
-     * Removes busy expired items.
+     * Cleans up the working queue with payloads that have been languishing there for too long. A specified timeout tells you how long a consumer can take in order to process an item. If that timeout is exceeded, it will mark that item as failed and then take the appropriate action. Either re-enqueue it, or remove it from the queue as trash.
      */
     void cleanup();
 
     /**
-     * Handle failed items.
+     * Collects failed items from the failed sub-queue and takes the appropriate action with these payloads.
      *
      * @return payoads of failed items
      */
